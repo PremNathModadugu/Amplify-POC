8cc010bfabe9c07ae373427af67b9cb5165fa4e3
PremNath-idexcel <62259682+PremNath-idexcel@users.noreply.github.com>
2022-07-11 16:09:34 -0400

    feat: cognito auth for application

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifypoc0e90f05e": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
